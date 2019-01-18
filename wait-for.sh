3cf51ab03710f38aba1747386ae163fd00922dc4
Aleksey <pshenichnov@gmail.com>
2019-01-18 14:10:42 -0500

    try to use another wait-for.sh script as the previous one didn't fit well

--- wait-for.sh
+++ wait-for.sh
@@ -1,73 +1,79 @@
 #!/bin/sh
-# shellcheck shell=dash
 
-#
-# Waits for the given host(s) to be available via TCP before executing a given
-# command.
-#
-# Usage: ./wait.sh [-t timeout] host:port [host:port] [...] [-- command args...]
-#
-# It accepts a number of `host:port` combinations to connect to via netcat.
-# The command to execute after each host is reachable can be supplied after the
-# `--` argument.
-# The default timeout of 10 seconds can be changed via `-t timeout` argument.
-#
-# Copyright 2016, Sebastian Tschan
-# https://blueimp.net
-#
-# Licensed under the MIT license:
-# https://opensource.org/licenses/MIT
-#
+TIMEOUT=15
+QUIET=0
 
-set -e
-
-TIMEOUT=10
-
-is_integer() {
-  test "$1" -eq "$1" 2> /dev/null
+echoerr() {
+  if [ "$QUIET" -ne 1 ]; then printf "%s\n" "$*" 1>&2; fi
 }
 
-connect_to_service() {
-  nc -w 1 -z "$1" "$2"
+usage() {
+  exitcode="$1"
+  cat << USAGE >&2
+Usage:
+  $cmdname host:port [-t timeout] [-- command args]
+  -q | --quiet                        Do not output any status messages
+  -t TIMEOUT | --timeout=timeout      Timeout in seconds, zero for no timeout
+  -- COMMAND ARGS                     Execute command with args after the test finishes
+USAGE
+  exit "$exitcode"
 }
 
-wait_for_service() {
-  local host="${1%:*}"
-  local port="${1#*:}"
-  local output
-  if ! is_integer "$port"; then
-    printf 'Error: "%s" is not a valid host:port combination.\n' "$1" >&2
-    return 1
-  fi
-  printf 'Waiting for %s to become available ... ' "$1" >&2
-  # shellcheck disable=SC2155
-  local timeout=$(($(date +%s)+TIMEOUT))
-  while ! output="$(connect_to_service "$host" "$port" 2>&1)"; do
-    if [ "$(date +%s)" -gt "$timeout" ]; then
-      echo 'timeout' >&2
-      if [ ! -z "$output" ]; then
-        echo "$output" >&2
+wait_for() {
+  for i in `seq $TIMEOUT` ; do
+    nc -z "$HOST" "$PORT" > /dev/null 2>&1
+    
+    result=$?
+    if [ $result -eq 0 ] ; then
+      if [ $# -gt 0 ] ; then
+        exec "$@"
       fi
-      return 1
+      exit 0
     fi
     sleep 1
   done
-  echo 'done' >&2
+  echo "Operation timed out" >&2
+  exit 1
 }
 
-while [ $# != 0 ]; do
-  if [ "$1" = '-t' ] || [ "$1" = '--timeout' ]; then
-    if ! is_integer "$2"; then
-      printf 'Error: "%s" is not a timeout integer.\n' "$2" >&2
-      exit 1
-    fi
+while [ $# -gt 0 ]
+do
+  case "$1" in
+    *:* )
+    HOST=$(printf "%s\n" "$1"| cut -d : -f 1)
+    PORT=$(printf "%s\n" "$1"| cut -d : -f 2)
+    shift 1
+    ;;
+    -q | --quiet)
+    QUIET=1
+    shift 1
+    ;;
+    -t)
     TIMEOUT="$2"
+    if [ "$TIMEOUT" = "" ]; then break; fi
     shift 2
-  fi
-  if [ "$1" = '--' ]; then
+    ;;
+    --timeout=*)
+    TIMEOUT="${1#*=}"
+    shift 1
+    ;;
+    --)
     shift
-    exec "$@"
-  fi
-  wait_for_service "$1"
-  shift
+    break
+    ;;
+    --help)
+    usage 0
+    ;;
+    *)
+    echoerr "Unknown argument: $1"
+    usage 1
+    ;;
+  esac
 done
+
+if [ "$HOST" = "" -o "$PORT" = "" ]; then
+  echoerr "Error: you need to provide a host and port to test."
+  usage 2
+fi
+
+wait_for "$@"
