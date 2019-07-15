9e9b9c756e94b957407f25b4b07ed48a094e1faa
Oleg Kuibar <kuibar.oleg@gmail.com>
2019-07-15 21:29:44 +0300

    add support of env variables to auth guards

--- src/app/core/services/auth/auth-profile-guard.guard.ts
+++ src/app/core/services/auth/auth-profile-guard.guard.ts
@@ -1,40 +1,49 @@
-import {Injectable} from '@angular/core';
-import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router} from '@angular/router';
-import {Observable, of} from 'rxjs';
-import {AuthService} from './auth.service';
-import {User} from 'oidc-client';
+import { Injectable } from '@angular/core';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Router,
+} from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { AuthService } from './auth.service';
+import { User } from 'oidc-client';
 import { map, catchError, flatMap } from 'rxjs/operators';
+import { environment } from 'environments/environment';
 
 @Injectable()
 export class AuthProfileGuardGuard implements CanActivate {
+  constructor(private auth: AuthService, private router: Router) {}
 
-  constructor(private auth: AuthService, private router: Router) {
-  }
-
-  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot,
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    if (!environment.capabilities.login) {
+      this.router.navigate(['/home']);
+      return false;
+    }
 
     return this.auth.getUser().pipe(
-      flatMap(
-      (user: User) => {
+      flatMap((user: User) => {
         if (user) {
-          return this.auth.askAboutProfile(user.profile).pipe(map(
-            (userResponse) => {
+          return this.auth.askAboutProfile(user.profile).pipe(
+            map(userResponse => {
               return true;
-            }
-          ), catchError(
-            (error) => {
+            }),
+            catchError(error => {
               this.router.navigate(['/home']);
               return of(false);
-            }
-          ));
+            }),
+          );
         } else {
           of(false);
         }
-      }), catchError(
-      (error) => {
+      }),
+      catchError(error => {
         this.router.navigate(['/home']);
         return of(false);
-      }
-    ));
+      }),
+    );
   }
 }
