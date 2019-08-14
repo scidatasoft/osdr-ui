e303762031b09bba928b79b33f8b7fbf1c090f82
Rick Zakharov <rick.zakharov@gmail.com>
2019-08-13 22:32:44 -0400

    Adjusting capabilities

--- src/environments/environment.local.ts
+++ src/environments/environment.local.ts
@@ -6,7 +6,7 @@
 export const environment = {
   name: 'LOCAL',
   production: false,
-  identityServerUrl: 'http://keycloak:8080/auth/realms/OSDR',
+  identityServerUrl: 'http://id.leanda.io/auth/realms/OSDR',
   // apiUrl: 'http://localhost:28610/api',
   apiUrl: 'http://localhost:28611/api',
   // apiUrl: 'https://api.dev.your-company.com/core/v1/api',
@@ -30,7 +30,7 @@ export const environment = {
     tabular: true,
     webPage: true,
     login: true,
-    fvc: true,
+    fvc: false,
     ssp: false,
     labwiz: false,
   },
